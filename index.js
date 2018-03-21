f570525114a2ed2825d24eaea8b824e652b1d57f
Darren Z M LIN <iamindianbbq33@gmail.com>
2018-03-21 19:39:32 +0800

    add http proxy

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 	function init(e){
 		let elements = document.getElementsByTagName("BODY");
 		elements[0].innerHTML = "<div>hello webpack with dll</div>";
-		alert($.fn.jquery);	
-		alert("hello again!!!!");	
+		//alert($.fn.jquery);	
+		$.ajax({
+			url:"/api",
+			method:"GET",
+		}).done((d)=>{
+			console.dir(d);
+		});
 	}
 	window.addEventListener("DOMContentLoaded",init);
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,30 @@
 const webpack = require("webpack");
 const path = require("path");
 const env = process.env.NODE_ENV
-
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const proxy = require('http-proxy-middleware');
+const convert = require('koa-connect');
+const Router = require('koa-router');
+const router = new Router();
+const proxyOptions = {
+  target: 'http://www.baidu.com',
+  changeOrigin: true
+};
+router.get('/api', convert(proxy(proxyOptions)));
 module.exports = {
     mode: env || 'development',
+    devtool:"inline-source-map",
     entry: {
-      app:__dirname + '/index.js',
-      vendors:path.join(__dirname,'dist',"vendors.js")
+        app: __dirname + '/index.js',
+        vendors: path.join(__dirname, 'dist', "vendors.js")
     },
     output: {
         path: __dirname + '/dist',
         filename: '[name].js',
-        publicPath: ''
+        publicPath: '/'
+    },
+    watchOptions:{
+        ignored: /node_modules/
     },
     module: {
         rules: [{
@@ -41,22 +54,35 @@ module.exports = {
                 query: {
                     limit: 8192
                 }
+            },
+
+            {
+                test: /\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
             }
+
         ]
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jQuery',
-            'window.$':'jQuery'
+            'window.$': 'jQuery'
         }),
         new webpack.DllReferencePlugin({
-           manifest: require(path.join(__dirname,'dist', 'vendors-manifest.json'))
+            manifest: require(path.join(__dirname, 'dist', 'vendors-manifest.json'))
         }),
+        new UglifyJSPlugin(),
     ],
-    serve:{
-      dev:{
-        publicPath:"/"
-      }
+    serve: {
+        dev: {
+            stats:"normal",
+        },
+        add: (app, middleware, options) => {
+                middleware.webpack();
+                middleware.content();
+                app.use(router.routes());
+                app.use(router.allowedMethods());
+        }
     }
-   
 }
